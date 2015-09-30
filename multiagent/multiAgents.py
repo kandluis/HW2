604714dbe7c391936808711be1ca9b5f92c557f5
Luis Perez <luisperez@college.harvard.edu>
2015-09-30 19:54:10 -0400

    Cleaned up code a little bit

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -213,38 +213,39 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         def isPacMan(agent):
             return agent % nagents == 0
 
-        def abMiniMax(state, agent, alpha, beta):
-          if terminal(state) or int(agent / nagents) >= self.depth:
-            return (None, self.evaluationFunction(state))
+        def abMiniMax(state, depth, alpha, beta):
+            agentIndex = depth % nagents
+            if terminal(state) or int(depth / nagents) >= self.depth:
+                return (None, self.evaluationFunction(state))
 
-          if isPacMan(agent):
-            #max's turn!
-            value = -float("Inf")
-            best_action = None
-            for action in state.getLegalActions(agent % nagents):
-              res = abMiniMax(state.generateSuccessor(agent % nagents, action), agent + 1, alpha, beta)[1]
-              if res > value:
-                value = res
-                best_action = action
-              if value > beta:
-                return (action, value)
-              alpha = max(alpha, value)
-            return (best_action, value)
-          else:
-            value = float("Inf")
-            best_action = None
-            for action in state.getLegalActions(agent % nagents):
-              res = abMiniMax(state.generateSuccessor(agent % nagents, action), agent + 1, alpha, beta)[1]
-              if res < value:
-                value = res
-                best_action = action
-              if value < alpha:
-                return (action, value)
-              beta = min(beta, value)
-            return (best_action, value)
+            if isPacMan(agentIndex):
+                #max's turn!
+                value = -float("Inf")
+                best_action = None
+                for action in state.getLegalActions(agentIndex):
+                    res = abMiniMax(state.generateSuccessor(agentIndex, action), depth + 1, alpha, beta)[1]
+                    if res > value:
+                        value = res
+                        best_action = action
+                    if value > beta:
+                        return (action, value)
+                    alpha = max(alpha, value)
+                return (best_action, value)
+            else:
+                value = float("Inf")
+                best_action = None
+                for action in state.getLegalActions(agentIndex):
+                    res = abMiniMax(state.generateSuccessor(agentIndex, action), depth + 1, alpha, beta)[1]
+                    if res < value:
+                        value = res
+                        best_action = action
+                    if value < alpha:
+                        return (action, value)
+                    beta = min(beta, value)
+                return (best_action, value)
 
-        return abMiniMax(gameState, 0, -float("Inf"), float("Inf"))[0]  
- 
+        return abMiniMax(gameState, 0, -float("Inf"), float("Inf"))[0]
+
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
